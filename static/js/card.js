e13471a2059e585ebbb0b4f4ccc59296a4bdfb86
tomhooijenga <tomhooijenga@gmail.com>
2016-02-04 18:41:42 +0100

    Added engineer card

---
+++ static/js/card.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+    /**
+     * Define a simple jQuery plugin that fills a details-card
+     * @param {{}} data An object with the key => values to fill
+     * @returns {jQuery}
+     */
+    $.fn.card = function (data) {
+
+
+
+        return this;
+    };
+})(jQuery);
