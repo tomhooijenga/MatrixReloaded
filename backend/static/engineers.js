e4c28efcff4c5f553c0cc49769d7a7212a2db96b
tomhooijenga <tomhooijenga@gmail.com>
2016-02-05 14:53:28 +0100

    Added auto-fill for a card

--- backend/static/engineers.js
+++ backend/static/engineers.js
@@ -3,34 +3,78 @@ $(document).ready(function () {
     var table = $('table').DataTable({
         "sAjaxSource": "/api/engineers/?format=json",
         "sAjaxDataProp": "results",
-        "bInfo" : false,
+        "bInfo": false,
         "bPaginate": false,
         "bFilter": false,
         // We initialize the column fields with the required details (First name, Last name) and add some HTML with the render function.
         "columns": [
-            {data: "first_name",
-            "className": 'details-control'},
+            {
+                data: "first_name",
+                "className": 'details-control'
+            },
             {data: "last_name"},
-            {render: function () {
+            {
+                render: function () {
                     return '<a class="note">Note</a>';
-                }, orderable: false},
-            {render: function () {
+                }, orderable: false
+            },
+            {
+                render: function () {
                     return '<a class="skills">Skills</a>';
-                }, orderable: false},
-            {render: function () {
+                }, orderable: false
+            },
+            {
+                render: function () {
                     return '<a class="edit">Edit</a>';
-                }, orderable: false}
+                }, orderable: false
+            }
         ]
     });
     // On click functions for the HTML elements in the DataTable
     // On click they should open the details panel on the right
-    $("table").on("click", ".note", function(){
+    $("table").on("click", ".note", function () {
         $("div.panel.panel-default.details").show();
     });
-    $("table").on("click", ".skills", function(){
+    $("table").on("click", ".skills", function () {
         $("div.panel.panel-default.details").show();
     });
-    $("table").on("click", ".edit", function(){
+    $("table").on("click", ".edit", function () {
         $("div.panel.panel-default.details").show();
+    });
+
+    // Fill the country and countries selector
+    $.getJSON('/api/countries/').then(function (data) {
+
+        data = data.map(function (country) {
+            return {
+                id: country.url,
+                text: country.name
+            }
+        });
+
+        $('#countries, #country').select2({
+            data: data
+        });
+    });
+
+    // Fill the languages selector
+    $.getJSON('/api/languages/').then(function (data) {
+        data = data.map(function (lang) {
+            return {
+                id: lang.url,
+                text: lang.name
+            }
+        });
+
+        $('#languages').select2({
+            data: data
+        });
+    });
+
+    // this part is just for testing
+    var $card = $('#card');
+
+    $.getJSON('/api/engineers/1/').then(function (data) {
+        $card.card(data);
     });
 });
--- static/js/card.js
+++ static/js/card.js
@@ -1,14 +1,60 @@
 (function ($) {
 
     /**
-     * Define a simple jQuery plugin that fills a details-card
-     * @param {{}} data An object with the key => values to fill
+     * A simple plugin to support the details card behaviour
+     * @param method
      * @returns {jQuery}
      */
-    $.fn.card = function (data) {
+    $.fn.card = function (method /*, arguments */) {
 
+        // If this method is defined, call it with the given arguments.
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1))
+        }
+        // Default to the 'init' method
+        else {
+            return methods.init.apply(this, arguments);
+        }
+    };
 
+    var methods = {
+        /**
+         *
+         * @param data Object with keys as input names and values as input values
+         * @returns {jQuery}
+         */
+        init: function (data) {
+            return this.each(function () {
+                var $this = $(this);
 
-        return this;
+                for (var key in data) {
+                    if (!data.hasOwnProperty(key)) continue;
+
+                    // Replace all _ with - then find that element in our current scope
+                    // Searches by class and by name
+                    var name = key.replace(/_/g, '-'),
+                        el = $this.find('.' + name + ', [name="' + name + '"]');
+
+                    el.each(function () {
+                        var $el = $(this),
+                            autofill = $el.data('autofill');
+
+                        if (autofill) {
+                            $el.prop(autofill, data[key]);
+                        } else {
+                            $el.val(data[key]);
+                        }
+                    });
+                }
+            });
+        },
+        /**
+         *
+         * @param enabled Are the elements enabled?
+         * @returns {jQuery}
+         */
+        editable: function (enabled) {
+            return this.find('input, select').prop('disabled', !enabled);
+        }
     };
 })(jQuery);
