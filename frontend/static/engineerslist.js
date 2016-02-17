ac7d9ae61133ada05c7a6a081f17f89dbadbcf48
erikstelt <erikstelt@gmail.com>
2016-02-17 15:40:20 +0100

    Country filter done
    - Added country filter
    - Added fixed date for cookie

--- frontend/static/engineerslist.js
+++ frontend/static/engineerslist.js
@@ -1,31 +1,69 @@
+// We can get cookies with this function
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+// This file lists the engineers on the page
 $(document).ready(function () {
-    // We initialize the DataTable with the json file required for the engineer page
-    var table = $('.engineerslist').DataTable({
-        "sAjaxSource": "/api/engineers/?format=json",
-        "sAjaxDataProp": "results",
-        "bInfo" : false,
-        "bPaginate": false,
-        // The part below makes our table scrollable when showing more than 16 items.
-        "deferRender": true,
-        "bScrollCollapse": true,
-        "scrollY": '56vh',
-        // We initialize the column fields with the required details (First name, Last name) and add some HTML with the render function.
-        "columns": [
-            {data: "first_name",
-            "className": 'details-control'},
-            {data: "last_name"},
-            {render: function () {
-                    return 'FSS / ASP';
-                }, orderable: false,
-                searchable: false},
-            {render: function () {
-                    return 'Country';
-                }, orderable: false,
-                searchable: false}
-        ]
+    var engineers = [];
+    function getArray(){
+        return $.getJSON("/api/engineers/?expand=country&format=json");
+    };
+    getArray().done(function(json) {
+        var cookie = getCookie("countries");
+        // Checks if cookie is empty
+        // If it's empty, it just adds the normal json request to an array.
+        if (cookie == "") {
+            $.each(json.results, function(key, val) {
+                engineers.push(val);
+            });
+        // If the cookie is not empty it filters the results.
+        } else {
+            var filter = cookie.split(",");
+            $.each(json.results, function(key, val) {
+                $.each(filter, function(code, country){
+                    if (val.country.code === country) {
+                        engineers.push(val);
+                    }
+                });
+            });
+        };
+        // We initialize the DataTable with the json file required for the engineer page
+        var table = $('.engineerslist').DataTable({
+            //"sAjaxSource": "/api/engineers/?expand=country&format=json",
+            //"sAjaxDataProp": "results",
+            "aaData": engineers,
+            "bInfo" : false,
+            "bPaginate": false,
+            // The part below makes our table scrollable when showing more than 16 items.
+            "deferRender": true,
+            "bScrollCollapse": true,
+            "scrollY": '56vh',
+            // We initialize the column fields with the required details (First name, Last name) and add some HTML with the render function.
+            "columns": [
+                {data: "first_name",
+                "className": 'details-control'},
+                {data: "last_name"},
+                {render: function () {
+                        return 'FSS / ASP';
+                    }, orderable: false,
+                    searchable: false},
+                {data: "country.code"}
+                //{render: function () {
+                //        return 'Country';
+                //    }, orderable: false,
+                //    searchable: false}
+            ]
+        });
+        // Makes the search input search-bar work on the DataTable
+        $('.search-bar').keyup(function(){
+            table.search($(this).val()).draw() ;
+        });
     });
-    // Makes the search input search-bar work on the DataTable
-    $('.search-bar').keyup(function(){
-        table.search($(this).val()).draw() ;
-    }); 
 });
--- frontend/static/filter.js
+++ frontend/static/filter.js
@@ -1,29 +1,68 @@
+// This function sets a Cookie with the selected Countries. They will have a long expire date so it will remember the preferences.
 function setCookie(countries){
-    var date = new Date("January 1, 2017");
+    var expireYear = (new Date).getFullYear() + 2;
+    var date = new Date("January 1, " + expireYear);
     var dateString = date.toGMTString();
     var cookieString = "countries=" + countries + ";expires=" + dateString;
     document.cookie = cookieString;
 }
-
+// We can get cookies with this function
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
 $(document).ready(function () {
-    $(".filterlanden").select2();
-    // Local bestand
+    // Sets the cookie for later use
+    var cookie = getCookie("countries");
+    // We get the JSON from the API.
     $.getJSON("/api/countries/?format=json", function (data) {
+        // We format an array since select2 can't use a JSON array
         var formatData = data.map(function(obj){ 
             var newObj = {};
             newObj.id = obj.code;
             newObj.text = obj.name;
             return newObj;
         });
-        $(".filterlanden").select2({
+        // Select2 shows all the countries in the datatabase
+        var element = $(".filterlanden").select2({
             data: formatData
         });
+        // Checks if cookie is set.
+        if (cookie != "") {
+            // If the cookie is set, we split it into an array
+            var filter = cookie.split(",");
+            // We loop through all the items in the array, and add the selected countries if they are saved in the cookie
+            for (var d = 0; d < formatData.length; d++) {
+                var item = formatData[d];
+                // Create the DOM option that is pre-selected by default
+                $.each(filter, function(code, country){
+                    if (country == item.id) {
+                        var option = new Option(item.text, item.id, true, true);
+                        // Append it to the select
+                        element.append(option);
+                        // Update the selected options that are displayed
+                        element.trigger('change');
+                    };
+                });
+            };
+        }
     });
+    // On click we create the cookie and reload the page to show the filtered results
     $(document).on("click", "#savefilt", function () {
-        var selectedCountries = $(".filterlanden").val().join(',');
-        setCookie(selectedCountries);
+        if ($(".filterlanden").val() == null) {
+            // This removes the cookie if no countries are selected
+            document.cookie = 'countries' + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+        } else {
+            var selectedCountries = $(".filterlanden").val().join(',');
+            setCookie(selectedCountries);
+        };
         $('.selectCountries').modal('hide');
-    });
-    
-    
+        location.reload(true);
+    })       
 });
