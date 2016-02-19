1d583e58df31db96e22fcef5b65addefe408cdd7
erikstelt <erikstelt@gmail.com>
2016-02-19 09:37:55 +0100

    revert davy merge

--- frontend/static/engineerslist.js
+++ frontend/static/engineerslist.js
@@ -1,39 +1,42 @@
+// This file lists the engineers on the page
 $(document).ready(function () {
+    var cookie = getCookie("countries");
+    // We will use this variable to create the JSON request
+    var jsonUrl = "";
+    // Checks if cookie is empty
+    // If it's empty, it requests the normal JSON url
+    if (cookie == "") {
+        jsonUrl = "/api/engineers/?expand=country";
+    } else {
+        // If the cookie is not empty it filters the results.
+        jsonUrl = "/api/engineers/?expand=country&countries=" + cookie;
+    };
     // We initialize the DataTable with the json file required for the engineer page
     var table = $('.engineerslist').DataTable({
-        "sAjaxSource": "/api/engineers/?format=json",
-        "sAjaxDataProp": "results",
-        "bInfo" : false,
+        ajax: {
+            url: jsonUrl,
+            dataSrc: ''
+        },
+        "bInfo": false,
         "bPaginate": false,
         // The part below makes our table scrollable when showing more than 16 items.
         "deferRender": true,
         "bScrollCollapse": true,
-        "scrollY": '56vh',
+        "scrollY": '45vh',
         // We initialize the column fields with the required details (First name, Last name) and add some HTML with the render function.
         "columns": [
             {data: "first_name",
-            "className": 'details-control'},
+                "className": 'details-control'},
             {data: "last_name"},
             {render: function () {
                     return 'FSS / ASP';
                 }, orderable: false,
                 searchable: false},
-            {render: function () {
-                    return 'Country';
-                }, orderable: false,
-                searchable: false}
+            {data: "country.code"}
         ]
     });
     // Makes the search input search-bar work on the DataTable
-    $('.search-bar').keyup(function(){
-        table.search($(this).val()).draw() ;
-    });
-
-        // table on click product
-    $('table tbody').on('click', 'tr.odd, tr.even', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row( tr );
-        table.ajax.reload();
-        console.log(table.row( this ).data() );
+    $('.search-bar').keyup(function () {
+        table.search($(this).val()).draw();
     });
 });
