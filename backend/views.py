3a604c85b2e9c721fe9f6767eeb4cfcd0cbb35f6
tomhooijenga <tomhooijenga@gmail.com>
2016-02-21 16:11:13 +0100

    Added admin index redirect

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from api.models import Engineer, Product, Category
+
+
+def index(request):
+    """
+    There is no index. Instead, redirect to the engineers page.
+    """
+    return redirect('backend-engineers', permanent=True)
+
 
 @login_required
 def engineers(request):
@@ -10,17 +18,20 @@ def engineers(request):
         'meta': Engineer._meta
     })
 
+
 @login_required
 def products(request):
     return render(request, 'products.html', {
         'meta': Product._meta
     })
 
+
 @login_required
 def categories(request):
     return render(request, 'categories.html', {
         'meta': Category._meta
     })
+
 
 @login_required
 def users(request):
