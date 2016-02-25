dc1d6ff237f67048503986ea3c27d69e6a3100a2
tomhooijenga <tomhooijenga@gmail.com>
2016-02-25 14:27:46 +0100

    Minor fixes

--- backend/static/backend.js
+++ backend/static/backend.js
@@ -14,19 +14,28 @@ $(function () {
         var url = this.files[0] ? URL.createObjectURL(this.files[0]) : '',
             $img = $('.image-input img');
 
-        // Revoke the old url.
-        URL.revokeObjectURL($img.prop('src'));
-
         $img.prop('src', url);
 
         // The dimensions of an image are sometimes incorrectly reported as 0x0. If we add
         // a tiny timeout, we can retrieve the correct size
         window.setTimeout(function () {
-            // Determine the type of image (landscape or portrait)
-            var isHorizontal = $img.width() > $img.height();
+            var width = $img.prop('naturalWidth'),
+                height = $img.prop('naturalHeight'),
+                type;
 
-            $img.removeClass('image-vertical image-horizontal')
-                .addClass(isHorizontal ? 'image-horizontal' : 'image-vertical');
+            $img.removeClass('image-vertical image-horizontal image-square');
+
+            if (width === height) {
+                type = 'image-square';
+            } else {
+                type = width > height ? 'image-horizontal' : 'image-vertical';
+            }
+
+            $img.addClass(type);
+
+            // Revoke the url directly, so the file it links to can be cleaned
+            //  up whenever the image's source changes
+            URL.revokeObjectURL(url);
         }, 50);
     });
 
